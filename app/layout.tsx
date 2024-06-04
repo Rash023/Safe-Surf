19493c68a4c93682091504f019ce21b57857c972
Rash023 <rashidmazhar0507@gmail.com>
2024-06-04 13:25:03 +0530

    config changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Safe Surf",
+  title: "Web Hawk",
   description: "Instant site safety checker",
 };
 
